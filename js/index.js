9b4e9f76fcf636143bd7c7e93ac8bbd42a29e339
Asrorbeck <2010288.asrorbek.tursunpulatov@gmail.com>
2025-09-28 20:14:19 +0500

    first

---
+++ js/index.js
@@ -0,0 +1,42 @@
+const targetDate = new Date("2025-10-29T00:00:00").getTime();
+
+const countdown = setInterval(function () {
+  const now = new Date().getTime();
+  const timeRemaining = targetDate - now;
+
+  if (timeRemaining <= 0) {
+    clearInterval(countdown);
+  } else {
+    const days = Math.floor(timeRemaining / (1000 * 60 * 60 * 24));
+    const hours = Math.floor(
+      (timeRemaining % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+    );
+    const minutes = Math.floor(
+      (timeRemaining % (1000 * 60 * 60)) / (1000 * 60)
+    );
+    const seconds = Math.floor((timeRemaining % (1000 * 60)) / 1000);
+
+    document.getElementById("days").innerText = days;
+    document.getElementById("hours").innerText = hours;
+    document.getElementById("minutes").innerText = minutes;
+    document.getElementById("seconds").innerText = seconds;
+  }
+}, 1000);
+
+const musicControl = document.getElementById("navbar__btn");
+const audio = document.getElementById("background-music");
+const musicIcon = document.getElementById("music-icon");
+
+let isPlaying = true;
+musicControl.addEventListener("click", function () {
+  if (isPlaying) {
+    audio.muted = false;
+    audio.play();
+    musicIcon.src = "./img/unmute.svg";
+  } else {
+    audio.pause();
+    musicIcon.src = "./img/mute.svg";
+  }
+
+  isPlaying = !isPlaying;
+});
